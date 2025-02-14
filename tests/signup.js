416ccbde8d501463765af77bff4c61600d3a3e9b
Aline Areda <77371831+AlineAreda@users.noreply.github.com>
2025-02-14 10:21:51 -0300

    Update signup.js

--- tests/signup.js
+++ tests/signup.js
@@ -9,20 +9,19 @@ export function handleSummary(data) {
   };
 }
 
-//definir virtual users e duration dos testes
-//thresholds chave de limite para duração de uma requisição
+
 export const options = {
   vus: 10,
   duration: "30s",
   thresholds: {
-    http_req_duration: ["p(95)<2000"], // 95% das requisições devem responder em até 2s
-    http_req_failed: ["rate<0.01"], //1% das req. podem ocorrer erros
+    http_req_duration: ["p(95)<2000"], 
+    http_req_failed: ["rate<0.01"], 
   },
 };
 
 export default function () {
   const url = "http://localhost:3333/signup";
-  //JSON modulo nativo do JS, stringfy função JS que transforma um objeto json em uma string mantendo o seu formato
+
 
   const payload = JSON.stringify({
     email: `${uuid.v4().substring(24)}@qa.e2ecoders.com`,
@@ -36,9 +35,8 @@ export default function () {
   };
 
   const res = http.post(url, payload, headers);
-  //console.log(res.body);
   check(res, {
-    "status should be 201": (r) => r.status === 201, //parse do objeto res
+    "status should be 201": (r) => r.status === 201, 
   });
   sleep(1);
 }
